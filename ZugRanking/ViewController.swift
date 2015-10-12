fc9c28bf4b060f7d6b61d6cd7b8360e0b9c3910b
ZugMensch <y2t.zugmensch@gmail.com>
2015-10-12 10:28:41 +0900

    Initial Commit

---
+++ ZugRanking/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ZugRanking
+//
+//  Created by 森下翔太 on 2015/10/12.
+//  Copyright © 2015年 ZuGMensch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ZugRankingTests/ZugRankingTests.swift
@@ -0,0 +1,36 @@
+//
+//  ZugRankingTests.swift
+//  ZugRankingTests
+//
+//  Created by 森下翔太 on 2015/10/12.
+//  Copyright © 2015年 ZuGMensch. All rights reserved.
+//
+
+import XCTest
+@testable import ZugRanking
+
+class ZugRankingTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
